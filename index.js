6d4552cae34375daef886b059aa63740bbdb0b7a
send4t <133424813+send4t@users.noreply.github.com>
2023-07-12 15:17:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
 import myJson from './example.json' assert {type: 'json'};
 
-
+poem.textContent = "this is a vers"
  
     function predict() {
     
-      var random =  Math.floor(Math.random() * images.length)
-      var image = images[random];
-      
-      // document.getElementById("theImage").src=image; // Replace first image with random picture
-      
-      var imgElement = document.createElement("img");
-      imgElement.src = image;
-      imgElement.width="350";
+     poem.textContent = "this is a vers after button pushed"
 
-      gif.innerHTML = ""; // Clear previous image
-      gif.appendChild(imgElement);
-     var result = document.getElementById("result");
-      result.innerHTML = ""; // Clear previous result
-       wanna.textContent = "this is number variable" +number
-      wannaa.textContent = "this is random variable" +random
-
-      if (number === random) {
-        result.textContent = "You won!";
-      } else {
-        result.textContent = "You lost!";
-      }
+     
     }
 
     
