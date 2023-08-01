25900fb8f070d677cccf5cf54d5f34b4adb98f3e
send4t <133424813+send4t@users.noreply.github.com>
2023-08-01 12:51:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-import random from 'random';
-
-const poems = require('./example.json');
-const poem = random.choice(poems);
-
-const poemDiv = document.getElementById('poem');
-poemDiv.innerHTML = poem;
+    
+async function getPoems() {
+  try {
+    const response = await fetch('https://extinct-jade-bedclothes.cyclic.app/');
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+getPoems();
